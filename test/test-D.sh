06b7cd15fb877c1b567115dc9d2775f1012bb384
Dongdong Tian <seisman.info@gmail.com>
2016-03-03 20:42:33 +0800

    test D option

---
+++ test/test-D.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+R=9/20/-2/2
+J=X15c/5c
+B=1
+PS=test-D.ps
+
+gmt pssac -R$R -J$J -B$B seis.sac -K -P > $PS
+gmt pssac -R$R -J$J -B$B seis.sac -Wred -K -O -D0.2c/0.2c >> $PS
+gmt psxy -R$R -J$J -T -K -O >> $PS
+rm gmt.*
