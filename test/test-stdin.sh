4c1525599e8e1caabdd2ed706ea6ae14770620db
Dongdong Tian <seisman.info@gmail.com>
2016-03-03 20:12:58 +0800

    test -h option

--- test/test-stdin.sh
+++ test/test-stdin.sh
@@ -6,8 +6,16 @@ By=y5
 PS=test-stdin.ps
 
 gmt pssac *.z -J$J -R$R -B$Bx -B$By -BWSen -Ed -M0.8i -K -P > $PS
-gmt pssac input.dat -J$J -R$R -B$Bx -B$By -BWsen -Ed -M0.8i -K -O -Y8c >> $PS
-gmt pssac -J$J -R$R -B$Bx -B$By -BWsen -Ed -M0.8i -K -O -Y8c >> $PS << EOF
+gmt pssac input.dat -J$J -R$R -B$Bx -B$By -BWsen -Ed -M0.8i -K -O -Y7c >> $PS
+gmt pssac -J$J -R$R -B$Bx -B$By -BWsen -Ed -M0.8i -K -O -Y7c >> $PS << EOF
+# This is a comment, it will be skipped
+ntkl.z 195 16 2p,red
+nykl.z 300 20 0p,blue
+onkl.z 400 30 2.2p,yellow
+sdkl.z 500 35 2.2p
+EOF
+gmt pssac -J$J -R$R -B$Bx -B$By -BWsen -Ed -M0.8i -K -O -Y7c -h1 >> $PS << EOF
+name    X  Y   pen
 ntkl.z 195 16 2p,red
 nykl.z 300 20 0p,blue
 onkl.z 400 30 2.2p,yellow
