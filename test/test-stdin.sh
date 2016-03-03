7bbe6266101466207a35a58a6983fb44961934e2
Dongdong Tian <seisman.info@gmail.com>
2016-03-03 20:10:40 +0800

    test reading from stdin

---
+++ test/test-stdin.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+R=195/1600/14/42
+J=X15c/6c
+Bx=x250
+By=y5
+PS=test-stdin.ps
+
+gmt pssac *.z -J$J -R$R -B$Bx -B$By -BWSen -Ed -M0.8i -K -P > $PS
+gmt pssac input.dat -J$J -R$R -B$Bx -B$By -BWsen -Ed -M0.8i -K -O -Y8c >> $PS
+gmt pssac -J$J -R$R -B$Bx -B$By -BWsen -Ed -M0.8i -K -O -Y8c >> $PS << EOF
+ntkl.z 195 16 2p,red
+nykl.z 300 20 0p,blue
+onkl.z 400 30 2.2p,yellow
+sdkl.z 500 35 2.2p
+EOF
+gmt psxy -J$J -R$R -O -T >> $PS
+rm gmt.*
